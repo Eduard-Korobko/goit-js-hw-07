228350c54aafb4a2ddd4b01b2c1d3f359d4c3698
Eduard-Korobko <96089313+Eduard-Korobko@users.noreply.github.com>
2023-12-09 22:35:34 +0200

    edit

--- js/task-01.js
+++ js/task-01.js
@@ -3,11 +3,7 @@ const allCategories = document.querySelector("#categories");
 const liItem = document.querySelectorAll(".item");
 console.log(`Number of categories: ${liItem.length}`);
 
-const category = document.querySelectorAll("h2");
-
-const elements = document.querySelector("ul").children;
-
-for (let i = 0; i < liItem.length; i++) {
-    console.log(`Category: ${category[i].textContent}`);
-    console.log(elements.length);
-}
+liItem.forEach((item) => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
+})
